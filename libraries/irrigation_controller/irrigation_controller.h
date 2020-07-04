ad3654404428ca5143edc4e12a9065419e5bb840
Kriston Costa <>
2020-07-04 14:47:58 -0400

    Last documentation

--- libraries/irrigation_controller/irrigation_controller.h
+++ libraries/irrigation_controller/irrigation_controller.h
@@ -3,6 +3,20 @@
 
 #include "Arduino.h"
 class Pulse {
+    /*
+     * A pulse has two parts, a frequency and a pulse length.
+     *
+     * The frequency is how often a pulse is fired, eg, a 5 second frequency would trigger a pulse every
+     * 5 seconds.
+     *
+     * The pulse is how long the pump will be on for, eg, a 3 second pulse will turn a pump on for 3 seconds
+     *
+     * So if you have a pulse that's 3 seconds long with a frequency of 5 seconds then the pulse will fire for 3
+     * seconds, then turn off for 2 seconds (the frequency counts down even while a pulse is active)
+     *
+     * Deactivating a pulse will immediately stop the pulse from firing.
+     *
+     */
 public:
     explicit Pulse(unsigned int pulse_length);
     void activate();
@@ -23,6 +37,15 @@ private:
 };
 
 class IrrigationController {
+    /*
+     * The IrrigationController polls the moisture pin to see if the moisture is below a
+     * threshold. If it is it activates it's internal pulse. When the internal pulse fires it turns on
+     * the associated pump pin. Once the moisture level reaches acceptable levels internal pulse is deactivated.
+     *
+     * The step method will perform all the above steps, enabling/disabling pumps as needed.
+     *
+     * Refer to step() in `irrigation_controller.cpp` to get implementation details.
+     */
 public:
     IrrigationController(int moisture_pin, int pump_pin);
     unsigned int getMoisture();
